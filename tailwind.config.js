90773c9746bba99d2544f80c0d7deb6f7124aa48
[Maxime CodeWeb] <[coquelin.maxime@yahoo.com]>
2022-11-11 18:36:56 +0100

    StartProject

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    colors: {
+      'colorFooter': '#2B2B35',
+      'white': '#FFFFFF',
+      'greyFooter': '#B5B5C2',
+    },
+    extend: {
+      fontFamily: {
+        'deca': ['"Lexend Deca"', 'modern']
+    },
+    lineHeight: {
+      'extra-loose': '2.5',
+      '12': '3rem',
+      'my': '6.5rem',
+    },
+    height: {
+      '200': '200px',
+    },
+    inset: {
+      'footer': '46.99rem',
+    },
+    spacing: {
+      'footer': '46.99rem',
+    }
+    },
+  },
+  plugins: [],
+}
